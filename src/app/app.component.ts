4eef6e9bb54593709af549b257c8bb3d937117ef
Riccardo Patalocchi <riccardo.patalocchi@gmail.com>
2025-04-19 22:06:58 +0200

    Navbar almost done if i can find a way to let sticky to work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
-  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [NavbarComponent, HomeComponent, FooterComponent]
+  
 })
 export class AppComponent {
   title = 'work-in-progress';
