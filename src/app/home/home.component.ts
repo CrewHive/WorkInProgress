9fc2c65ac0473ed560442b2ddcec1d91386f2316
Riccardo Patalocchi <riccardo.patalocchi@gmail.com>
2025-04-24 09:16:48 +0200

    Title animation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+    repeatAnimationo(): void {
+        const element = document.getElementById("wip");
+        if (element) {
+            element.classList.remove('animate__animated');
+            void element.offsetWidth; // Trigger reflow
+            element.classList.add('animate__animated');
+        }
+    }
 }
