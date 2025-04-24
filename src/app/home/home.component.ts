8e0d27d2c77822210ce98941fab1eb76e1fb504a
Riccardo Patalocchi <riccardo.patalocchi@gmail.com>
2025-04-24 10:36:05 +0200

    Carousel without css

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,26 @@
+import { NgFor, NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss'],
+  imports: [NgFor, NgClass]
 })
+
 export class HomeComponent {
 
-    repeatAnimationo(): void {
-        const element = document.getElementById("wip");
-        if (element) {
-            element.classList.remove('animate__animated');
-            void element.offsetWidth; // Trigger reflow
-            element.classList.add('animate__animated');
-        }
-    }
+  readonly carouselItems: string[] = ["miao", "miao2", "miao3"];
+
+  currentIndex: number = 0;
+
+  next(): void {
+    this.currentIndex = (this.currentIndex + 1) % this.carouselItems.length;
+  }
+
+  prev(): void {
+    this.currentIndex = (this.currentIndex - 1 + this.carouselItems.length) % this.carouselItems.length;
+
+  }
 }
